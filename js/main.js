4f7c7b8f4fe42ed6c4f657198753cab992f47f9e
jamludovic <ludovic@jamendo.com>
2016-07-03 13:03:06 +0200

    hamburger to x example working

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 $(function() {
 
-    var menuButton = $('#menu-button');
-	var menuOverlay = $('#menu-overlay');
+    var menuButton = $('.menu-button');
+	var menuOverlay = $('.menu-overlay');
 
 	menuButton.on('click', function() {
 
-		$(this).toggleClass('is-morphed');
-		menuOverlay.toggleClass('is-opened');
+		$(this).toggleClass('is-active');
+		//menuOverlay.toggleClass('is-opened');
 
 	});
 
