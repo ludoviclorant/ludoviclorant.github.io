b311be64509a8b47cc8b05c078c90112e8f7de97
jamludovic <ludovic@jamendo.com>
2016-07-03 12:07:15 +0200

    menu in progress, added jquery

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-document.createElement("picture");
+$(function() {
+
+    var menuButton = $('#menu-button');
+	var menuOverlay = $('#menu-overlay');
+
+	menuButton.on('click', function() {
+
+		$(this).toggleClass('is-morphed');
+		menuOverlay.toggleClass('is-opened');
+
+	});
+
+});
